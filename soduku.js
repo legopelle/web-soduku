dc7e54f8a2f44faf5ad323db2a313bdd603e3eb0
Per Engström <per.olov.engstrom@gmail.com>
2015-04-14 15:12:49 +0200

    Basic css and table generation
    The webpage now renders a generated 9x9 table acceptably. Generated
    with JS and styled with CSS.

--- soduku.js
+++ soduku.js
@@ -1,1 +1,29 @@
 /* jshint:strict */
+
+var body = document.getElementsByTagName("body")[0];
+
+var tbl = document.createElement("table");
+var tblBody = document.createElement("tbody");
+
+for (i=0; i < 9; i++) {
+  row = document.createElement("tr");
+
+  for (j=0; j < 9; j++) {
+
+    var value = Math.floor(10 * Math.random());
+    console.log(value);
+    console.log(value.toString());
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(value.toString());
+
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+  }
+
+  tblBody.appendChild(row);
+
+}
+
+tbl.appendChild(tblBody);
+body.appendChild(tbl);

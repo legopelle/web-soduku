457f1a1fe761e5384b98831c2cbb24deb297b3bb
Per Engström <per.olov.engstrom@gmail.com>
2015-04-14 15:49:18 +0200

    Table colors
    Now uses color to distinguish subtables.

--- soduku.js
+++ soduku.js
@@ -11,11 +11,13 @@ for (i=0; i < 9; i++) {
   for (j=0; j < 9; j++) {
 
     var value = Math.floor(10 * Math.random());
-    console.log(value);
-    console.log(value.toString());
+    //console.log(value);
+    //console.log(value.toString());
 
     cell = document.createElement("td");
     cellText = document.createTextNode(value.toString());
+    
+    cell.style.backgroundColor = isGrey(i, j) ? "LavenderBlush" : "Gainsboro";
 
     cell.appendChild(cellText);
     row.appendChild(cell);
@@ -27,3 +29,14 @@ for (i=0; i < 9; i++) {
 
 tbl.appendChild(tblBody);
 body.appendChild(tbl);
+
+function isGrey(x, y) {
+  var r = Math.floor(x / 3.0);
+  var c = Math.floor(y / 3.0);
+
+  if (Math.pow(-1, r + c)  == 1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
